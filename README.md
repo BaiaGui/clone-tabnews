58cb6535609b0d78b969cfdd69b7a9ac83def7ee
Guilherme Baia Venâncio <106357203+BaiaGui@users.noreply.github.com>
2024-12-01 17:03:01 +0000

    add prettier scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do tabnews.com.br o objetivo de aprender e praticar o desenvolvimento de aplicações ponta a ponta
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Você é uma pessoa muito especial e inteligente!</h1>
+function Home() {
+  return <h1>Você é uma pessoa muito especial e inteligente!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
