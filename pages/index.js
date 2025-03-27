41eb6b8fc0f44d5c3517100d411d1f983f67c128
Guilherme Baia Venâncio <106357203+BaiaGui@users.noreply.github.com>
2025-03-27 23:40:29 +0000

    change index message (testing)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Você é uma pessoa muito especial e inteligente!</h1>;
+  return <h1>Você é uma pessoa muito especial e inteligente! (no staging)</h1>;
 }
 
 export default Home;
