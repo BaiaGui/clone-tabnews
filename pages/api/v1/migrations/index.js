49e687a4a9450471d082bd48f0beff7bdbc5bf7b
Guilherme Baia Venâncio <106357203+BaiaGui@users.noreply.github.com>
2025-08-08 00:19:25 +0000

    refactor: refactor `api/v1/migrations` controller

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,42 +1,65 @@
+import { createRouter } from "next-connect";
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 
-export default async function migrations(req, res) {
-  const allowedMethods = ["GET", "POST"];
-  if (!allowedMethods.includes(req.method)) {
-    return res.status(405).json({ error: `Method ${req.method} not allowed` });
-  }
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
-    if (req.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      res.status(200).json(pendingMigrations);
-    }
-    if (req.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
+const router = createRouter();
 
-      if (migratedMigrations.length > 0) {
-        res.status(201).json(migratedMigrations);
-      }
-      res.status(200).json(migratedMigrations);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await dbClient.end();
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
+
+let dbClient;
+
+async function getHandler(req, res) {
+  const [dbClient, defaultMigrationOptions] = await configMigrationOptions();
+  const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+  await dbClient.end();
+  res.status(200).json(pendingMigrations);
+}
+
+async function configMigrationOptions() {
+  dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  return [dbClient, defaultMigrationOptions];
+}
+
+async function postHandler(req, res) {
+  const [dbClient, defaultMigrationOptions] = await configMigrationOptions();
+  const migratedMigrations = await migrationRunner({
+    ...defaultMigrationOptions,
+    dryRun: false,
+  });
+
+  if (migratedMigrations.length > 0) {
+    res.status(201).json(migratedMigrations);
   }
+  res.status(200).json(migratedMigrations);
+  await dbClient.end();
+}
+
+function onNoMatchHandler(req, res) {
+  const methodNotAllowedError = new MethodNotAllowedError();
+  res.status(methodNotAllowedError.statusCode).json(methodNotAllowedError);
+}
+
+async function onErrorHandler(error, req, res) {
+  console.log("Erro dentro do next-connect");
+  await dbClient?.end();
+  const publicErrorObject = new InternalServerError({ cause: error });
+  console.log(publicErrorObject);
+  res.status(500).json(publicErrorObject);
 }
