88e1e210e87257c67382bf939b6d1cacc93acbb3
Guilherme Baia Venâncio <106357203+BaiaGui@users.noreply.github.com>
2025-08-09 19:44:34 +0000

    feat: create new `ServiceError` custom error

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,11 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ statusCode, cause }) {
     super("Unexpected Error Ocurred.", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Please, contact the support";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
   }
 
   toJSON() {
@@ -24,6 +24,26 @@ export class MethodNotAllowedError extends Error {
     this.name = "MethodNotAllowedError";
     this.action = "Verify the HTTP method used";
     this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ message, cause }) {
+    super(message || "Service temporarily unavailable.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Please, verify if the service is available";
+    this.statusCode = 503;
   }
 
   toJSON() {
